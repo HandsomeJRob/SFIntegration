db22a8adc9ba119ad465d7d54e983e8c28e623e8
Jonathan <jonathan.d.robertson1985@gmail.com>
2019-03-22 17:42:13 -0600

    workflow outbound message works, added logout

--- SFIntegration/Program.cs
+++ SFIntegration/Program.cs
@@ -12,18 +12,47 @@ namespace SFIntegration
     {
         private static string _sessionId;
         private static string _metadataUrl;
+        private static string _enterpriseUrl;
 
         static void Main(string[] args)
         {
+            // Note: user used for sync must have api access.  
+            // It may be wise to create a new Salesforce user with appropriate permissions, 
+            // to be used for sync purposes.
+
+            // inputs needed from user
+            // 1. Username
+            // 2. Password
+            // 3. Token for user (see https://help.salesforce.com/articleView?id=user_security_token.htm&type=5)
+            
+            // inputs coming from somewhere... config?
+            // 1. Workflow rule name
+            // 2. Workflow rule description (optional)
+            // 3. Workflow rule formula (condition for sync)
+            // 4. Workflow action name
+            // 5. Workflow action description (optional)
+            // 6. Workflow action callout endpoint
+            // 7. Names of fields we want to sync on Opportunity
+            // 8. API version (currently 45)
+            // 9. Activate now?
+
+
             LoginResult loginResult = Login();
 
             _sessionId = loginResult.sessionId;
             _metadataUrl = loginResult.metadataServerUrl;
+            _enterpriseUrl = loginResult.serverUrl;
 
-            // WORKS...
-            //CreateNewObject("MetadataObject1");
+            try
+            {
+                CreateNewObject("MetadataObject1");
 
-            CreateWorkflow();
+                CreateWorkflow();
+            }
+            finally
+            {
+                Logout();
+            }
         }
 
         private static LoginResult Login()
@@ -36,6 +65,7 @@ namespace SFIntegration
 
                 // Salesforce uses password + token to authenticate external systems
                 // To get a token... https://help.salesforce.com/articleView?id=user_security_token.htm&type=5
+                // https://blog.mkorman.uk/integrating-with-metadata-api/
                 string password = "$uckyPass1";
                 string token = "lDGd3rMQtLHJJazKnjRYUHcu";
                 string loginPassword = password + token;
@@ -47,31 +77,64 @@ namespace SFIntegration
             return loginResult;
         }
 
-        //private static void Logout(string sessionId, string url)
-        //{
-        //    var soapClient = new SoapClient("Soap", url);
-        //    var result = soapClient.logout(new JRSandbox.SessionHeader {  sessionId = _sessionId })
+        private static void Logout()
+        {
+            var soapClient = new SoapClient("Soap", _enterpriseUrl);
 
-        //    //var result = soapClient.logout();
-        //    Console.WriteLine($"Logged out of session ID {sessionId}");
-        //}
+            var result = soapClient.logout(new JRSandbox.SessionHeader { sessionId = _sessionId }, null);
+
+            Console.WriteLine($"Logged out of session ID {_sessionId}");
+        }
 
         private static void CreateWorkflow()
         {
+            var wfAction = new WorkflowOutboundMessage
+            {
+                name = "JROpportunityAction",
+                fullName = "Opportunity.JROpportunityAction",
+                endpointUrl = "https://prod-17.centralus.logic.azure.com:443/workflows/8ce4281445fe47e695a4ee65d14913cd/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=7K9iP14qF4cy-dTTfz3PMKQn9h5Ll3qzFQ-t1Pyjw3I",
+                integrationUser = "jonathan.d.robertson1985@gmail.com",
+                description = "Check out this action",
+                apiVersion = 45,
+                fields = new string[]
+                {
+                    "AccountId",
+                    "Amount",
+                    "Id",
+                    "CloseDate",
+                    "IsWon",
+                    "Name",
+                    "Type",
+                    "OrderNumber__c"
+                }
+            };
+
             // Notes:
             // Fullname - must match ObjectName.RuleName
             // Formula - FieldName <operator> value
 
             var wfRule = new WorkflowRule
             {
-                fullName = "Opportunity.MetaDataRule",
+                fullName = "Opportunity.JRMetaDataRule",
                 description = "Created from metadata API",
                 triggerType = WorkflowTriggerTypes.onAllChanges,
                 formula = "Amount > -1",
                 active = false,
+                actions = new WorkflowActionReference[]
+                {
+                    new WorkflowActionReference
+                    {
+                        name = "JROpportunityAction",
+                        type = WorkflowActionType.OutboundMessage
+                    }
+                }
             };
 
-            SendMetadata(new Metadata[] { wfRule });
+            SendMetadata(new Metadata[]
+            {
+                wfAction,
+                wfRule
+            });
         }
 
 
@@ -115,6 +178,8 @@ namespace SFIntegration
             var sessionHeader = new JRSandbox.Metadata.SessionHeader { sessionId = _sessionId };
 
             var result = metaClient.createMetadata(sessionHeader, null, null, metadata);
+
+            bool success = result.All(r => r.success);
         }
     }
 }
